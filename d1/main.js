307e8c7417fa330053fc9a6a02123ff845461cf6
Roland smallDell <rodondera@gmail.com>
2022-05-14 10:26:06 +0200

    Completed work on the day one

--- d1/main.js
+++ d1/main.js
@@ -18,13 +18,25 @@ const buttons = document.querySelector(".buttons");
 
 buttons.addEventListener("click", (e) => {
     if (e.target.classList.contains("add")) {
-        console.log("Add");
+        count.innerHTML++;
+        setColor()
     }
     if (e.target.classList.contains("subtract")) {
-        console.log("Subtract");
+        count.innerHTML--;
+        setColor()
     }
     if (e.target.classList.contains("reset")) {
-        console.log("Reset");
+        count.innerHTML = 0;
+        setColor()
     }
 })
 
+function setColor() {
+    if (count.innerHTML > 0) {
+        count.style.color = "yellow";
+    } else if (count.innerHTML < 0) {
+        count.style.color = "orangered";
+    } else {
+        count.style.color = "#fff";
+    }
+}
