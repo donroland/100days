1405b0888dab3da1bdf1c8e911d9441a68918fa1
Roland bigDell <rodondera@gmail.com>
2022-05-15 21:09:02 +0200

    Completed work on the day four

--- d4/main.js
+++ d4/main.js
@@ -1,1 +1,20 @@
-console.log("Alles gut");
+const modal = document.querySelector(".modal");
+const btn = document.querySelector(".container__button");
+const close = document.querySelector(".close");
+
+btn.addEventListener("click", openModal);
+close.addEventListener("click", closeModal);
+modal.addEventListener("click", closeModal);
+
+// open modal
+
+function openModal(e) {
+    e.preventDefault();
+    modal.style.display = "block";
+}
+
+// close modal
+
+function closeModal() {
+    modal.style.display = "none";
+}
