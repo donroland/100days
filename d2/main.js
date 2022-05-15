c5c214fb88fcb967b26ba9995f818a511ea2255d
Roland bigDell <rodondera@gmail.com>
2022-05-15 11:05:15 +0200

    Completed work on the day two

---
+++ d2/main.js
@@ -0,0 +1,14 @@
+const number = document.querySelector(".number");
+const button = document.querySelector(".generate");
+
+const generateNumber = () => {
+    const randomNumber = Math.floor(Math.random() * 10 + 1);
+    number.innerHTML = randomNumber;
+}
+
+button.addEventListener("click", generateNumber);
+
+generateNumber();
+
+// const rand = Math.floor(Math.random() * 10 + 1);
+// console.log(rand);
