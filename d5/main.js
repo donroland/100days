985ded8ccc54cc790ec121dcf1e739f12870541b
Roland bigDell <rodondera@gmail.com>
2022-05-16 11:44:20 +0200

    Completed work on the day five

---
+++ d5/main.js
@@ -0,0 +1,25 @@
+const btn = document.querySelector(".btn");
+const result = document.querySelector(".result");
+
+btn.addEventListener("click", palindrome);
+
+function palindrome() {
+    const word = document.querySelector(".input-text").value;
+    let len = word.length;
+
+    let start = word.substring(0, Math.floor(len/2)).toLowerCase();
+    let end = word.substring(len - Math.floor(len/2)).toLowerCase();
+
+    // let flip = end.split("").reverse().join("");
+
+    let flip = [...end].reverse().join("");
+    // console.log(start);
+    // console.log(end);
+    // console.log(flip);
+
+    if (start == flip) {
+        result.innerText = "This word is a palindrome";
+    } else {
+        result.innerText = "This word is NOT a palindrome";
+    }
+}
