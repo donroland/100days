eab3006b4044a46302a742bb4467981e2b95e763
Roland bigDell <rodondera@gmail.com>
2022-05-16 14:26:36 +0200

    Correction on day five

--- d5/main.js
+++ d5/main.js
@@ -4,19 +4,19 @@ const result = document.querySelector(".result");
 btn.addEventListener("click", palindrome);
 
 function palindrome() {
-    const word = document.querySelector(".input-text").value;
+    const word = document.querySelector(".input-text").value.replace(/\s/g, "");
     let len = word.length;
-
+   
     let start = word.substring(0, Math.floor(len/2)).toLowerCase();
+
     let end = word.substring(len - Math.floor(len/2)).toLowerCase();
 
     // let flip = end.split("").reverse().join("");
-
-    let flip = [...end].reverse().join("");
+    let flip = [...end].reverse().join("").trim();
     // console.log(start);
     // console.log(end);
     // console.log(flip);
-
+    
     if (start == flip) {
         result.innerText = "This word is a palindrome";
     } else {
