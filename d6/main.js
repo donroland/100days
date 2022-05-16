a32d3ad9d37082924b61141f3b60166acfca80e3
Roland bigDell <rodondera@gmail.com>
2022-05-16 15:00:52 +0200

    Completed work on the day six

---
+++ d6/main.js
@@ -0,0 +1,18 @@
+const btn = document.querySelector(".btn");
+const result = document.querySelector(".result");
+const word = document.querySelector(".input-text");
+
+btn.addEventListener("click", countVowel);
+
+function countVowel() {
+    let vowelCount = 0;
+    let wordVal = word.value.toLowerCase();
+    
+    for (let i = 0; i <wordVal.length; i ++){
+        let letter = wordVal[i];
+        if (letter.match(/([a,e,i,o,u])/)) {
+            vowelCount++;
+        }
+    }
+    result.innerText = `Podana fraza ${word.value} posiada ${vowelCount} samogłosek`;
+}
